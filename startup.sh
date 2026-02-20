625e722ff6eec6692e089541c30b31a9dc0395c4
Saim Momin <mominsaim12@gmail.com>
2026-02-20 09:26:31 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-# Set up CORS headers for the static file server
-cd /opt/dot
+# Symlink staged inputs so the web server can serve them via URL
+ln -s /home/inputs /opt/dot/inputs
 
-# Start simple HTTP server with CORS enabled
-python3 -m http.server 8080 --bind 0.0.0.0
+# Build URL parameters
+COORDS_PARAM="?coords=inputs/input.coords&index=inputs/input.coords.idx"
+ANNOTATION_PARAM=""
+
+if [ -f /home/inputs/annotation.bed ]; then
+    ANNOTATION_PARAM="&annotations=inputs/annotation.bed"
+fi
+
+URL_SUFFIX="${COORDS_PARAM}${ANNOTATION_PARAM}"
+
+# Inject auto-redirect into index.html (only redirects if no params already in URL)
+sed -i "s|<script type=\"text/javascript\">|<script type=\"text/javascript\">\n\t\tif (!window.location.search) { window.location.search = '${URL_SUFFIX}'; }|" /opt/dot/index.html
+
+# Start the web server
+cd /opt/dot
+python3 -m http.server 8080
